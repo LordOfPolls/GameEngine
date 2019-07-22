7d5d568d51a635e5c9b1427608c5be753e39b2cd
LordOfPolls <ddavidallen13@gmail.com>
2019-07-22 11:03:48 +0100

    Idiot-proofing

--- run.bat
+++ run.bat
@@ -1,2 +1,11 @@
 @echo off
+echo Verifying python is installed
+WHERE pythons
+IF %ERRORLEVEL% NEQ 0 start "" https://www.python.org/ftp/python/3.5.4/python-3.5.4-amd64.exe
+
+echo Checking and installing dependencies
+python -m pip install -r requirements.txt --quiet
+
+echo Dependencies should be installed...
+echo Booting Game Engine
 python main.py
